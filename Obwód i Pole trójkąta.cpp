b0c5d01d64eda1253f680af36ae4b70157ae04b5
Greeeg111 <110864624+Greeeg111@users.noreply.github.com>
2023-02-21 22:07:58 +0100

    Add files via upload

---
+++ Obwód i Pole trójkąta.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b,h;
+cout << "Podaj Długość boku  a";
+cin >> a;
+cout << "Podaj Długość boku b";
+cin >> b;
+cout << "Podaj Długość wysokości";
+cin >> h;
+cout << "Obwód Wynosi " << 1*a+2*b;
+cout << "cm";
+cout << "Pole Prostokąta wynosi " << a*b+h;
+cout << "cm";
+
+ return 0;
+}
